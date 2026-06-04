35ab30035caaad40cc252e9ad67fb42b11344ad2
Vino <nvinothin21@gmail.com>
2026-06-04 15:29:30 +0530

    test null resource11

--- network.tf
+++ network.tf
@@ -401,7 +401,7 @@ resource "null_resource" "bastion_to_private_test" {
       "ping -c 1 google.com",
       "mkdir -p ~/.ssh",
       "cat > ~/.ssh/private_key <<'EOF'\n${var.ssh_private_key}\nEOF",
-      "chmod 600 ~/.ssh/private_key"
+      "chmod 600 ~/.ssh/private_key",
 
       "ssh -o StrictHostKeyChecking=no -i ~/.ssh/private_key opc@${oci_core_instance.application_node1.private_ip} hostname"
     ]
