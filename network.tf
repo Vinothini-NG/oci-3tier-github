38178af511227d7f7d292162b88c32af327fb225
Vino <nvinothin21@gmail.com>
2026-06-04 11:29:36 +0530

    added bastion host1

--- network.tf
+++ network.tf
@@ -88,6 +88,21 @@ resource "oci_core_subnet" "public_subnet" {
 }
 
 #create bastion host
+#CREATE BASTION HOST
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.tenancy_ocid
+}
+
+data "oci_core_images" "oracle_linux" {
+  compartment_id           = var.compartment_ocid
+  operating_system         = "Oracle Linux"
+  operating_system_version = "9"
+  shape                    = "VM.Standard.E5.Flex"
+
+  sort_by    = "TIMECREATED"
+  sort_order = "DESC"
+}
+
 resource "oci_core_instance" "bastion_host" {
   availability_domain = "eaWm:AP-SYDNEY-1-AD-1"
   compartment_id      = var.compartment_ocid
